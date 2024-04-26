c03fba8cba777872bf9b3ca193fd06844a7bdaf7
oriolorriols <oriol.orriols@gmail.com>
2024-04-26 21:18:51 +0200

    Added Logo to Login Page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,24 +4,31 @@ const Login = () => {
   return (
     <>
         <div className='flex justify-center items-center h-screen'>
-            <div className='card'>
+            <div className=''>
 
-             <div className='mb-5'>
-                 <label className="block" htmlFor="username">Username</label>
-                 <input className="p-2 mt-1" type="text" placeholder='Enter Username'/>
-             </div>
+                <div className="flex justify-center mb-5">
+                  <img className="justify-center" src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Apple_logo_grey.svg" width="50px" alt="" />
+                </div>
             
-            <div className='mb-5'>
-                <label className="block" htmlFor="password">Password</label>
-                <input className="p-2 mt-1" type="password" placeholder='Enter Password'/>
-            </div>
-         
-             <input className="mr-2" type="checkbox" />
-             <label htmlFor="remember">Remember me</label>
+                <div className='card '>
+                    <div className='mb-5'>
+                        <label className="block" htmlFor="username">Username</label>
+                        <input className="p-2 mt-1" type="text" placeholder='Enter Username'/>
+                    </div>
+                    
+                    <div className='mb-5'>
+                        <label className="block" htmlFor="password">Password</label>
+                        <input className="p-2 mt-1" type="password" placeholder='Enter Password'/>
+                    </div>
+                
+                    <input className="mr-2" type="checkbox" />
+                    <label htmlFor="remember">Remember me</label>
 
-             <div className='mb-5 text-center'>
-                <button className='bg-sky-900 rounded-md p-2 m-2'>Login</button>
-             </div>
+                    <div className='mt-3 text-center'>
+                        <button className='bg-sky-900 rounded-md p-2 m-2'>Login</button>
+                    </div>
+
+                </div>
 
             </div>
         </div>
