8e9558ae99925ce231aa44ccf05aa3186b1078ab
oriolorriols <oriol.orriols@gmail.com>
2024-06-09 10:58:28 +0200

    Updated TokenModal

--- src/components/modal/modalToken.jsx
+++ src/components/modal/modalToken.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
-import { Modal } from 'antd'
-
+import { Modal, Button } from 'antd';
 import { useAuth } from "../../contexts/authContext";
 import { useNavigate } from 'react-router-dom';
 
-
 const TokenModal = ({ visible }) => {
-
-    const { setLogOut } = useAuth()
-    const navigate = useNavigate()
+    const { setLogOut } = useAuth();
+    const navigate = useNavigate();
 
     const onOk = () => {
         setLogOut();
         navigate('/login');
-    }
+    };
 
-  return (
-    <Modal
-      title={<span><ExclamationCircleOutlined style={{ color: 'red' }} /> Token is expired. Please Log In again</span>}
-      open={visible}
-      onOk={onOk}
-      closable={false}
-      okText="OK"
-    >
-    </Modal>
-  );
+    return (
+        <Modal
+            title={<span><ExclamationCircleOutlined style={{ color: 'red' }} /> Token is expired. Please Log In again</span>}
+            open={visible}
+            onOk={onOk}
+            closable={false}
+            okText="OK"
+            footer={[
+                <Button key="ok" type="primary" onClick={onOk}>OK</Button>,
+            ]}
+            transitionName="zoom" 
+        />
+    );
 };
 
 export default TokenModal;
