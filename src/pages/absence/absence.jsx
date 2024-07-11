c7af3ccec6a99f4120da2eafb0ede09f77ec2f4d
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-07-11 21:39:59 +0200

    filtro usuario logeado en crear viaje

--- src/pages/absence/absence.jsx
+++ src/pages/absence/absence.jsx
@@ -12,7 +12,6 @@ const Absences = () => {
     const [dummy, refresh] = useState(false);
     const [openEdit, setOpenEdit] = useState(false);
     const { userId, isHR } = useAuth()
-    const { userLogged, setUserLogged } = useState({})
 
     const getAllAbsences = async () => {
         try {
@@ -25,37 +24,29 @@ const Absences = () => {
                 setAllAbsences(notRemoved)
             }
         } catch (error) {
-            setError(error.message);
+            console.log(error);
         }
     };
 
     const getAllUsers = async () => {
-        const users = await getUsers();
-        const notRemoved = users.filter((user) => !user.removeAt);
-        if (users.length) setAllUsers(notRemoved);
-        else setError(users.message);
+        try {
+            const users = await getUsers();
+            const notRemoved = users.filter((user) => !user.removeAt);
+            if (isHR !== "HR") {
+                const userLoggedId = notRemoved.filter((user) => user._id === userId)
+                setAllUsers(userLoggedId[0])
+            } else {
+                setAllUsers(notRemoved)
+            }
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    const getLoggedUser = async () => {
-        const data = await getOneUser(userId)
-        try {
-            setUserLogged(data)
-            console.log("cargando usuario en get logged user")
-        } catch (error) {
-            console.error("Error fetching logged user: ", error)
-        }
-    }
-
     useEffect(() => {
-        if (userId) {
-            if (isHR !== "HR") {
-                getLoggedUser();
-                console.log("funciona get logged user en sue effect si no eres HR")
-            }
-            getAllAbsences();
-            getAllUsers();
-            console.log(userLogged)
-        }
+        getAllAbsences();
+        getAllUsers();
+        console.log("all users ", allUsers)
     }, [dummy, userId]);
 
     const openCreateAbsence = () => {
@@ -67,7 +58,7 @@ const Absences = () => {
         <>
             <Flex wrap justify="space-between" align="flex-start">
                 <div className="title-box">
-                    <h1 className='title'>Lista de viajes</h1>
+                    <h1 className='title'>Lista de viajes {allUsers?.name}</h1>
                     <h2 className='subtitle'>Listado de todos los viajes por cada continente</h2>
                 </div>
             </Flex>
@@ -82,7 +73,6 @@ const Absences = () => {
                     onCancel={() => setOpenEdit(false)}
                     allUsers={allUsers}
                     refresh={refresh}
-                    idUser={userLogged}
                 />
             </Space>
             <Space wrap size={16} align="start">
