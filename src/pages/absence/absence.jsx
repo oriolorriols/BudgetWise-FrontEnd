d48e84d412f32fe8896206a2340496bccf939da2
oriolorriols <oriol.orriols@gmail.com>
2024-07-15 21:26:47 +0200

    Revert "Changes fectch"
    This reverts commit bbc22fc2273bc88f34764930099122521d316d03.

--- src/pages/absence/absence.jsx
+++ src/pages/absence/absence.jsx
@@ -53,7 +53,6 @@ const Absences = () => {
     const openCreateAbsence = () => {
         setOpenEdit(true);
         setSelectedAbsence(null)
-        console.log("Abre modal de crear")
     }
 
     return (
